6f97e21c283ee5e9d57eb7012ce87419a26a2e1e
Janis Jansen <janis.jansen@gmx.de>
2022-07-22 23:07:54 +0200

    Page title added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,31 @@
+import Head from "next/head";
 import Header from "@components/ui/Header";
 import styles from "@styles/modules/Home.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles["app-home"]}>
-      <Header />
-      <div className={styles["app-home__content"]}>
-        <h1>Nice to see you, friend!</h1>
-        <h2>
-          However, this website is just being built. If you want to, you can
-          come back in a couple of weeks?
-        </h2>
-        <h2>
-          Until then, please checkout my{" "}
-          <a href="https://github.com/carnageous" rel="noopener">
-            Github profile
-          </a>{" "}
-          :)
-        </h2>
+    <>
+      <Head>
+        <title>Janis.md</title>
+      </Head>
+
+      <div className={styles["app-home"]}>
+        <Header />
+        <div className={styles["app-home__content"]}>
+          <h1>Nice to see you, friend!</h1>
+          <h2>
+            However, this website is just being built. If you want to, you can
+            come back in a couple of weeks?
+          </h2>
+          <h2>
+            Until then, please checkout my{" "}
+            <a href="https://github.com/carnageous" rel="noopener">
+              Github profile
+            </a>{" "}
+            :)
+          </h2>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
